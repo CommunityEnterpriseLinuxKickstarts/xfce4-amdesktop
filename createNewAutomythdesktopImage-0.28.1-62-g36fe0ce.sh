b837c5755135c34506e9ffdf8964e33bfffbc920
SuperKMG <kelsieflynn@users.noreply.github.com>
2017-12-21 19:51:38 -0500

    Add files via upload

---
+++ createNewAutomythdesktopImage-0.28.1-62-g36fe0ce.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+BUILD=9;#++
+nextBUILD=$((++BUILD))
+DATESTAMP=`date +%Y%m%d`
+NAME=automythdesktop
+MYTHVER=0.28.1
+MYTHGITREL=62-g36f
+
+if [ ! -d ./LOGS ];then
+mkdir -v ./LOGS
+fi
+nohup livecd-creator -v -c $NAME-$MYTHVER-$MYTHGITREL.cfg -f $NAME-$MYTHVER-$MYTHGITREL-$BUILD -t $NAME-$MYTHVER-$MYTHGITREL-$DATESTAMP-r$BUILD --product $NAME-$MYTHVER-$MYTHGITREL-$DATESTAMP-r$BUILD  --tmpdir=./$NAME-$MYTHVER-$MYTHGITREL-$DATESTAMP-r$BUILD-TMPDIR --cache=$NAME-$MYTHVER-$MYTHGITREL-$DATESTAMP-r$BUILD-CACHEDIR > ./LOGS/$NAME-$MYTHVER-$MYTHGITREL-$DATESTMP-r$BUILD.nohup.log  && sed -i "/#++$/s/=.*#/=$nextBUILD;#/" ${0} &
